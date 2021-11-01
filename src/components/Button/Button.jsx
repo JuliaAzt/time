1f564aee03a64dcc3baef9bea853afc95d6c9949
Matheus Alencar <matheus.alencar@avitaseg.com.br>
2021-11-01 12:49:21 -0300

    Create button component

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import "./Button.scss";
+import PropTypes from "prop-types";
+
+const Button = (props) => {
+  function handleClassName() {
+    return `button button__${props.buttonType} ${
+      !!props.additionalClass ? props.additionalClass : ""
+    }`;
+  }
+
+  return (
+    <>
+      <button
+        className={handleClassName()}
+        type={props.type}
+        onClick={props.onClick}
+        disabled={props.disabled}
+      >
+        {props.children}
+      </button>
+    </>
+  );
+};
+
+Button.propTypes = {
+  type: PropTypes.oneOf(["button", "submit", "reset"]),
+  disabled: PropTypes.bool,
+  onClick: PropTypes.func,
+  buttonType: PropTypes.oneOf(["primary"]),
+  additionalClass: PropTypes.string,
+  children: PropTypes.node.isRequired,
+};
+
+Button.defaultProps = {
+  type: "button",
+  disabled: false,
+  onClick: () => {},
+  buttonType: "primary",
+  additionalClass: "",
+};
+
+export default Button;
